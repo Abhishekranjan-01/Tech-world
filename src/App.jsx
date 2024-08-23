e622a8fcfb36c29b790d8b75a9fae373ef40f935
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-08-23 07:27:42 +0530

    React query provider added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { Button } from "flowbite-react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { NavbarMenu } from "./components";
-import { Blogs, Home } from "./pages";
-import SignupForm from "./components/form/Signup.form";
+const queryClient = new QueryClient();
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavbarMenu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/" element={<SignupForm />} /> */}
-        <Route path="/blogs" element={<Blogs />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <>
+        <AppRoutes />
+      </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavbarMenu } from "../components";
+import Home from "../pages/Home";
+import Blogs from "../pages/Blogs";
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <NavbarMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<SignupForm />} /> */}
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
