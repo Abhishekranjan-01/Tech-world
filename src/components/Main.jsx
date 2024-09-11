f7dd802f5b34dddf971ea42dbfd4c8086c52db50
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-09-11 08:00:59 +0530

    Editor added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,17 @@ import { Button } from "flowbite-react";
 import blogData from "../blogsData.json";
 import { Link } from "react-router-dom";
 import { Alert } from "./common";
-import { useState } from "react";
-import { invokeNotify } from "./common/alert/Alert";
+import { useState, useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 export function Main() {
-  console.log(blogData);
   const [alert, setAlert] = useState(false);
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
   const notify = (label) => toast(label);
   return (
     <main
@@ -20,6 +25,43 @@ export function Main() {
       <button type="button" onClick={() => setAlert(true)}>
         Set Alert
       </button>
+      <button onClick={() => {}}>Write blogs</button>
+      <Editor
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
       <p className={"text-2xl font-semibold text-white"}>{String(alert)}</p>
     </main>
   );
